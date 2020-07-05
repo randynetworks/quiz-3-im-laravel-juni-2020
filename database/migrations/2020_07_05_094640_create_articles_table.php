8b4a8606940de93b7181edf432a24e71562409fd
randynetworks <randynetworks@gmail.com>
2020-07-05 17:04:34 +0700

    menyelesaikan add file controller dan model + ERD

---
+++ database/migrations/2020_07_05_094640_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_user');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('isi', 512);
+            $table->string('tag', 256);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
